d2a1a19356a54839b7ae660056addc5922490003
Rahul singh <rahulsinghmp968@gmail.com>
2018-09-07 19:20:41 +0530

    Initial commit

---
+++ app/src/main/java/singh/rahul/com/hell/MainActivity.java
@@ -0,0 +1,13 @@
+package singh.rahul.com.hell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
